b63ede8efdd2072982c0b01fbe43aa0f6519ee0f
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2022-10-10 22:45:10 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (66.45%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -24,31 +24,11 @@ class Solution {
 
 
 
-// If the string's length is odd, we can not change the character in the middle of the string, the string will remain palindrome.
+/*If the string's length is odd, we can not change the character in the middle of the string, the string will remain palindrome.
 
-// Since the first half and the second half of the palindrome string are symmetric, so we only need to be traverse the first half.
+Since the first half and the second half of the palindrome string are symmetric, so we only need to be traverse the first half.
 
-// While traverse the first half of the string, if the character is not a, we can replace it with a, and then return the result directly.
+While traverse the first half of the string, if the character is not a, we can replace it with a, and then return the result directly.
 
-// If the first half of the string are all a, the second half is also a, so just change the last character to b directly.
-
-
-// class Solution {
-//     public String breakPalindrome(String palindrome) {
-//         if (palindrome == null || palindrome.length() <= 1) {
-//             return "";
-//         }
-
-//         int len = palindrome.length();
-//         char[] array = palindrome.toCharArray();
-//         for (int i = 0; i < len / 2; i++) {
-//             if (array[i] > 'a') {
-//                 array[i] = 'a';
-//                 return new String(array);
-//             }
-//         }
-
-//         array[len - 1] = 'b';
-//         return new String(array);
-//     }
-// }
+If the first half of the string are all a, the second half is also a, so just change the last character to b directly.
+*/
