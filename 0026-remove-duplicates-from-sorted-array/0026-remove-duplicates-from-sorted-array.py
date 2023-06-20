08a016aad4cdaed77fcbf555d6839e46175880e1
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2023-06-20 13:46:07 +0530

    Time: 107 ms (34.98%), Space: 18.1 MB (16.95%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -2,11 +2,18 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         slow, fast = 0, 1
         while fast in range(len(nums)):
-            if nums[slow] == nums[fast]:
+            if nums[slow] == nums[fast]: ##If both slow and fast are equal, it means a duplicate element is found, so the fast pointer is incremented to move to the next element.
                 fast += 1
             else:
-                nums[slow+1] = nums[fast]
+                nums[slow+1] = nums[fast] ##If the elements are not equal, it means a unique element is found. In this case, the next position after slow is updated with the value of the element at index fast.
+                #Then both pointers, fast and slow, are incremented by 1 to move to the next elements.
                 fast += 1
                 slow += 1
 
-        return slow + 1
+        return slow + 1 #Finally, the function returns slow + 1, which represents the length of the resulting list without duplicates.
+    
+    
+"""
+The slow pointer represents the last unique element found so far, and the fast pointer represents the current element being checked.
+
+"""
