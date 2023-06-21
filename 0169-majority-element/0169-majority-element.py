454e6e9a454d8d6766c8ce08db8938ba11120ffb
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2023-06-21 21:21:12 +0530

    Time: 169 ms (81.30%), Space: 18 MB (28.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,6 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[(len(nums))//2]
+        nums.sort() #sort the elements 
+        return nums[(len(nums))//2] #If number is present more than n/2 times then it will be present at n/2
             
         
