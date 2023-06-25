f839e4d3d4ca2f3c3a5c14ebfe8e27b553986410
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2023-06-25 21:49:08 +0530

    Time: 144 ms (53.25%), Space: 17.3 MB (82.73%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -3,9 +3,9 @@ class Solution:
         
         curr_farthest = curr_end = jumps = 0 
         for i in range(len(nums)-1) :
-            curr_farthest = max(curr_farthest, i + nums[i])
-            if i == curr_end :
-                jumps += 1 
-                curr_end = curr_farthest
+            curr_farthest = max(curr_farthest, i + nums[i]) #max of the farthest elements that we can reach
+            if i == curr_end : #we have exhausted all min paths that we can get 
+                jumps += 1 #increment jumps to the next index
+                curr_end = curr_farthest #update the current farthest element value in curr
         
         return jumps
