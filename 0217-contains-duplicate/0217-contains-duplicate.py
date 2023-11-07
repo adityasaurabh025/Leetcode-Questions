05431c436c7983a1281015424612ec34ba02433e
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2023-11-07 23:23:03 +0530

    Time: 472 ms (69.33%), Space: 30.9 MB (68.92%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,11 +1,28 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        sorted_list= nums.sort()
-        length=len(nums)
-        for num in range(length-1):
-            if nums[num]==nums[num+1]:
-                return True
-        return False
+            return len(nums) > len(set(nums))
+
+#Algo
+'''
+Sort the list
+iterate through the list
+If first element is equal to second - then it has twice elements
+return true
+else
+return false
+
+Demerits:-
+we will have to iterate through all the list if it has very large number
+
+so, convert the list into set and 
+'''
+ 
+#         sorted_list= nums.sort()
+#         length=len(nums)
+#         for num in range(length-1):
+#             if nums[num]==nums[num+1]:
+#                 return True
+#         return False
         
         
         
