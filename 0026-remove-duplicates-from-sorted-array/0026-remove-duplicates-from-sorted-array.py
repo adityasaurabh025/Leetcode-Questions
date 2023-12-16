99323f3a3880227827910ec91e606ea5314925d3
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2023-12-16 18:47:07 +0530

    Time: 82 ms (74.19%), Space: 17.9 MB (45.38%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,5 +1,17 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        nums[:] = list(set(nums)) #nums[:] is known as list slicing, and it refers to the entire list nums. The [:] slice notation essentially creates a copy of the entire list.
-        nums.sort()
-        return len(nums)
+        # nums[:] = list(set(nums)) #nums[:] is known as list slicing, and it refers to the entire list nums. The [:] slice notation essentially creates a copy of the entire list.
+        # nums.sort()
+        # return len(nums)
+        
+        if not nums:
+            return 0
+
+        k = 1  # Pointer to track the position of unique elements
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:  # If current element is different from previous
+                nums[k] = nums[i]  # Move the unique element to the correct position
+                k += 1  # Increment the pointer
+
+        return k
