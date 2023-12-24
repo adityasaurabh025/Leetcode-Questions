e810091a4c384bbab2f7dc5d98f15e3ba8d59f95
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2023-12-24 20:30:41 +0530

    Time: 140 ms (92.29%), Space: 18.9 MB (5.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,17 +1,18 @@
 class Solution:
     def majorityElement(self, arr: List[int]) -> int:
+        # arr.sort()
+        # count=0
+        # majority=(len(arr)//2)
+        # for i in range(len(arr)-1):
+        #     if arr[i]==arr[i+1]:
+        #         count+=1
+        #         if count>majority:
+        #             return arr[i]
+        #     else:
+        #         count=1
+        # return arr[0]
         arr.sort()
-        count=0
-        majority=(len(arr)//2)
-        for i in range(len(arr)-1):
-            if arr[i]==arr[i+1]:
-                count+=1
-                if count>majority:
-                    return arr[i]
-            else:
-                count=1
-        return arr[0]
-            
+        return arr[(len(arr)//2)]#If number is present more than n/2 times then it will be present at n/2
         
         
         
