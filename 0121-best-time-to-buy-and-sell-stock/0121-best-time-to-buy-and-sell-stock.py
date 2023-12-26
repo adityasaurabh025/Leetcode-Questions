6e7895210f6c0f28bb2d5fe5daa746c466d387ed
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2023-12-26 23:35:57 +0530

    Time: 732 ms (98.62%), Space: 28.3 MB (6.40%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,27 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         #use Two pointer and keep track of max profit
-        left=0
-        right=1
-        max_profit_so_far=0
-        while right<len(prices):
-            current_profit=prices[right]-prices[left]
-            if prices[right]> prices[left]:
-                max_profit_so_far= max(current_profit, max_profit_so_far)
-            else:
-                left=right
-            right+=1
-        return max_profit_so_far
+        # left=0
+        # right=1
+        # max_profit_so_far=0
+        # while right<len(prices):
+        #     current_profit=prices[right]-prices[left]
+        #     if prices[right]> prices[left]:
+        #         max_profit_so_far= max(current_profit, max_profit_so_far)
+        #     else:
+        #         left=right
+        #     right+=1
+        # return max_profit_so_far
+        min_price = float('inf')
+        max_profit = 0
     
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+    
+        return max_profit
     
 #     class Solution:
 #     def maxProfit(self,prices):
