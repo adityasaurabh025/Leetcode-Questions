f907c8a40d3691cf77ba0294d96b4ab6a343fc5c
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2023-12-26 23:51:26 +0530

    Time: 751 ms (97.91%), Space: 28 MB (7.70%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -12,28 +12,17 @@ class Solution:
         #         left=right
         #     right+=1
         # return max_profit_so_far
-        min_price = float('inf')
-        max_profit = 0
-    
-        for price in prices:
-            if price < min_price:
-                min_price = price
-            elif price - min_price > max_profit:
-                max_profit = price - min_price
-    
+        
+        if not prices:
+            return 0
+        max_profit=0
+        min_price=prices[0]
+        
+        for i in range(1, len(prices)):
+            if prices[i]<min_price:
+                min_price=prices[i]
+            else:
+                max_profit= max(max_profit, prices[i]-min_price)
         return max_profit
-    
-#     class Solution:
-#     def maxProfit(self,prices):
-#         left = 0 #Buy
-#         right = 1 #Sell
-#         max_profit = 0
-#         while right < len(prices):
-#             currentProfit = prices[right] - prices[left] #our current Profit
-#             if prices[left] < prices[right]:
-#                 max_profit =max(currentProfit,max_profit)
-#             else:
-#                 left = right
-#             right += 1
-#         return max_profit
+
                 
