6727f8f0d4fc559f34de0f74198aeefc1c5a7e65
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2023-12-29 13:28:59 +0530

    Time: 507 ms (20.46%), Space: 18.5 MB (14.64%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,10 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        max_reach=nums[0]
+        max_reach=nums[0] #Initialize maximum reachable element to first index
         
-        for i in range(1, len(nums)):
-            if max_reach<i:
+        for i in range(1, len(nums)): # Fhir index 1 se loop kro end tak
+            if max_reach<i: # agar max_reach less that hai index se then false or can't reach
                 return False
             else:
-                max_reach=max(max_reach, nums[i]+i)
+                max_reach=max(max_reach, nums[i]+i) #max_reach me max daal do curr max ka aur index aur element ko add krke jo aaye wo
         return True
