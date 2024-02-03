cfaa8d2943c656340cd78c7ace9b80890ad0697b
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-02-04 00:47:35 +0530

    Time: 43 ms (23.87%), Space: 16.6 MB (58.77%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -13,3 +13,22 @@ class Solution:
                 return False
 
         return not stack
+    
+    """
+    Loop through each character in the string (for char in s):
+
+The code iterates through each character in the input string s.
+Check if the character is an open bracket (if char in brackets.values()):
+
+If the current character char is one of the open brackets (values in the brackets dictionary), it means an open bracket is encountered.
+In this case, the open bracket is pushed onto the stack.
+Check if the character is a close bracket (elif char in brackets.keys()):
+
+If the current character char is one of the close brackets (keys in the brackets dictionary), it means a close bracket is encountered.
+The code checks if the stack is not empty (if not stack) and if the top of the stack contains the corresponding open bracket.
+If the conditions are met, it means a matching pair of brackets is found, and the open bracket is popped from the stack.
+Check for Misplaced or Unmatched Brackets (if not stack or stack.pop() != brackets[char]):
+
+If the stack is empty (not stack), or the top of the stack does not contain the expected open bracket for the current close bracket, it indicates a misplacement or unmatched bracket.
+In such cases, the function immediately returns False, indicating that the input string is not valid.
+    """
