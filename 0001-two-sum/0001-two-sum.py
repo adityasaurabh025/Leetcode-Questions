435c11048a1dc4d89602cd24c1612b60ca5925de
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-02-03 14:09:45 +0530

    Time: 68 ms (51.84%), Space: 17.8 MB (65.30%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,41 @@
 class Solution:
     def twoSum(self, nums, target):
-        sorted_nums = sorted(enumerate(nums), key=lambda x: x[1])
-        left, right = 0, len(nums) - 1
-
-        while left < right:
-            curr_sum = sorted_nums[left][1] + sorted_nums[right][1]
-            if curr_sum == target:
-                return [sorted_nums[left][0], sorted_nums[right][0]]
-            elif curr_sum < target:
-                left += 1
-            else:
-                right -= 1
-        return []
+        
+        #Brute Force
+        # ans= [0,0]
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i]+nums[j]==target:
+        #             return [i, j]
+        # TC:- O(n*2), SC:- O(1)
+        
+        #hashMap
+        hashmap={}
+        for i in range(len(nums)):
+            diff= target-nums[i]
+            if diff in hashmap:
+                return [i, hashmap[diff]]
+            hashmap[nums[i]]=i
+        # TC:- O(n), SC:- O(n)
+        
+        #Approach 3, sorting
+        '''
+        Sort the array.
+Use two pointers approach - one starting from the beginning and the other from the end.
+Move the pointers based on whether the sum is less than or greater than the target.
+This approach has a time complexity of O(n log n) due to sorting and a space complexity of O(1).
+        '''
+        # nums_with_index = [(num, i) for i, num in enumerate(nums)]
+        # left, right = 0, len(nums) - 1
+        # while left < right:
+        #     current_sum = nums_with_index[left][0] + nums_with_index[right][0]
+        #     if current_sum == target:
+        #         return [nums_with_index[left][1], nums_with_index[right][1]]
+        #     elif current_sum < target:
+        #         left += 1
+        #     else:
+        #         right -= 1
+        # return []
         
         
         
