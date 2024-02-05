eb38480ea1826ac60c5207c24102940b39794a1c
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-02-05 14:11:46 +0530

    Time: 273 ms (87.33%), Space: 16.5 MB (85.90%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,15 +1,43 @@
 class Solution:
+    def expand_from_center(left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left + 1:right]
     def longestPalindrome(self, s: str) -> str:
         if len(s)<=1:
             return s
+        def expand_from_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+        max_str = s[0]
+
+        for i in range(len(s) - 1):
+            odd = expand_from_center(i, i)
+            even = expand_from_center(i, i + 1)
+
+            if len(odd) > len(max_str):
+                max_str = odd
+            if len(even) > len(max_str):
+                max_str = even
+
+        return max_str
+    
+        # maxLen=0 # max Length of string
+        # maxStr=s[0] #max String found so far
         
-        maxLen=0
-        maxStr=s[0]
-        
-        for i in range(len(s)-1):
-            for j in range(i+1, len(s)):
-                if j-i+1> maxLen and s[i:j+1]==s[i:j+1][::-1]:
-                    maxLen= j-i+1
-                    maxStr= s[i:j+1]
+#         for i in range(len(s)-1): 
+#             for j in range(i+1, len(s)):
+#                 if j-i+1> maxLen and s[i:j+1]==s[i:j+1][::-1]:
+#                     #check max length and reverse of that length if both condition satisfies then palindrome found
+#                     maxLen= j-i+1
+#                     maxStr= s[i:j+1]
                     
-        return maxStr
+#         return maxStr
+#     # TC:O(n*3), TC:-O(1)
+
+
+    
+    
