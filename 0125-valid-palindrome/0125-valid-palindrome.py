381513758da824dd5380e8fa0d0194b2d550839e
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-04-21 21:33:28 +0530

    Time: 39 ms (86.00%), Space: 18.2 MB (18.71%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,8 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # s=s.lower()
+        s = re.sub(r'[^a-zA-Z0-9]', '', s.lower())
+        
+        # Check if the string is a palindrome
+        return s == s[::-1]
