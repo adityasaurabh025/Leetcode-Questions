6c5a31d4fa86d318b82ff93cb7ad0cd9f62b4aeb
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-04-29 14:27:03 +0530

    Time: 123 ms (75.29%), Space: 18.1 MB (78.29%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -2,13 +2,18 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        """
-        idx=0
         
+        idx=0
         for i in range(len(nums)):
-            if nums[i]!=0:
-                nums[idx]=nums[i]
+            if nums[i]!=0: # check all non-zero elements and add it in the array
+                nums[idx]=nums[i] 
                 idx+=1
-        while idx< len(nums):
+        while idx < len(nums): #fill the array with zero untill idx reaches length of nums in the end
             nums[idx]=0
             idx+=1
+            """
+        zeroPos = 0
+        for cur in range(len(nums)):
+            if nums[cur] != 0:
+                nums[cur], nums[zeroPos] = nums[zeroPos], nums[cur]
+                zeroPos += 1
