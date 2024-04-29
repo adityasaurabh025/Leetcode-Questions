0e4f39a02edc44dc064dc32b73b0175105f03752
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-04-29 14:00:29 +0530

    Time: 164 ms (43.69%), Space: 28 MB (69.65%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,9 +5,19 @@ class Solution:
         """
         n = len(nums)
         k = k % n  # To handle cases where k > n
-    
-    # Rotate the array in-place using slicing
-        nums[:] = nums[-k:] + nums[:-k]
+        reverseArr(nums, 0, n-1) # reverse entire array
+        reverseArr(nums, 0, k-1) # reverse first k elements from start
+        reverseArr(nums, k, n-1) # reverse remaining elements from k till end
+        
+def reverseArr(nums, start, end ):
+    while start< end:
+        nums[start], nums[end]= nums[end], nums[start]
+        start+=1
+        end-=1
+        
+        
+#     # Rotate the array in-place using slicing
+#         nums[:] = nums[-k:] + nums[:-k]
             
         
         
