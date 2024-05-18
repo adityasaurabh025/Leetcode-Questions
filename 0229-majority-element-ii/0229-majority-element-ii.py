26de9f294c3ff60aa902fdee527c20c74d330948
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-05-18 14:29:29 +0530

    Time: 96 ms (72.20%), Space: 17.8 MB (95.66%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,24 +1,39 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        if not nums:
-            return []
-
-        nums.sort()
-        result = []
-        count = 1
-        n = len(nums)
-    
-        for i in range(1, n):
-            if nums[i] == nums[i - 1]:
-                count += 1
+        mp={}
+        res=[]
+        n=len(nums)
+        
+        for num in nums:
+            if num in mp:
+                mp[num]+=1
             else:
-                if count > n // 3:
-                    result.append(nums[i - 1])
-                count=1
+                mp[num]=1
+        for num, count in mp.items():
+            if count > n // 3:
+                res.append(num)
     
-    # Handle the last element
-        if count > n // 3:
-            result.append(nums[-1])
+        return res
+#         if not nums:
+#             return []
+
+#         nums.sort()
+#         result = []
+#         count = 1
+#         n = len(nums)
     
-        return result
+#         for i in range(1, n):
+#             if nums[i] == nums[i - 1]:
+#                 count += 1
+#             else:
+#                 if count > n // 3:
+#                     result.append(nums[i - 1])
+#                 count=1
+    
+#     # Handle the last element
+#         if count > n // 3:
+#             result.append(nums[-1])
+    
+#         return result
+            
             
