08c192e5f0577dac66cb2dbc24e2774b249aea89
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-05-19 15:30:05 +0530

    Time: 46 ms (15.63%), Space: 16.7 MB (7.58%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,7 +4,31 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        nums1[m:]=nums2
-        nums1.sort()
-        return nums1
+        # nums1[m:]=nums2
+        # nums1.sort()
+        # return nums1
+        # # TC:- O((m+n)log(m+n)), SC:- O(1)
+        
+        p1 = m - 1
+        p2 = n - 1
+        p = m + n - 1
+
+    # While there are elements in both arrays
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+
+    # If there are remaining elements in nums2, copy them
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
+        return p
+    
+    
         
