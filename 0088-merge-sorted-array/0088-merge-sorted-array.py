9503d349ce2a247726c28a72f18f2126430caf74
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-05-19 15:39:21 +0530

    Time: 42 ms (40.96%), Space: 16.6 MB (78.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -9,26 +9,26 @@ class Solution:
         # return nums1
         # # TC:- O((m+n)log(m+n)), SC:- O(1)
         
-        p1 = m - 1
-        p2 = n - 1
-        p = m + n - 1
+        left = m - 1
+        right = n - 1
+        curr = m + n - 1
 
-    # While there are elements in both arrays
-        while p1 >= 0 and p2 >= 0:
-            if nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
+        # While there are elements in both arrays
+        while left >= 0 and right >= 0:
+            if nums1[left] > nums2[right]:
+                nums1[curr] = nums1[left]
+                left -= 1
             else:
-                nums1[p] = nums2[p2]
-                p2 -= 1
-            p -= 1
+                nums1[curr] = nums2[right]
+                right -= 1
+            curr -= 1
 
     # If there are remaining elements in nums2, copy them
-        while p2 >= 0:
-            nums1[p] = nums2[p2]
-            p2 -= 1
-            p -= 1
-        return p
+        while right >= 0:
+            nums1[curr] = nums2[right]
+            right -= 1
+            curr -= 1
+        return curr
     
     
         
