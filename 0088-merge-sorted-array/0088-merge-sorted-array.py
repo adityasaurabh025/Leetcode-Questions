28091e270df67ebee1de8b4f4c46a5d80cb3dc3d
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-06-07 13:55:50 +0530

    Time: 33 ms (91.18%), Space: 16.6 MB (68.55%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -38,24 +38,17 @@ class Solution:
         # return nums1
         # # TC:- O((m+n)log(m+n)), SC:- O(1)
         
-        left = m - 1 # last element of nums1
-        right = n - 1 #last element of nums2
+        left = m - 1 # last element of nums1, 3
+        right = n - 1 #last element of nums2 , 6
         curr = m + n - 1 #last element of combined array
 
         # While there are elements in both arrays
         while left >= 0 and right >= 0:
             if nums1[left] > nums2[right]: 
-                #0>6, which is false, loop will go to else block
-                #0>5, which is false, loop will go in else block
-                #0>2, which is false, loop will go in else block
-                #3>2, true, below code will execute, []
                 nums1[curr] = nums1[left]
                 left -= 1
             else:
                 nums1[curr] = nums2[right]
-                #In first iteration, [0,0,0,0,0,6]
-                #In second iteration, [0,0,0,0,5,6]
-                #In third iteration, [0,0,0,2,5,6]
                 right -= 1
             curr -= 1
 
@@ -65,6 +58,48 @@ class Solution:
             right -= 1
             curr -= 1
         return curr
+    
+    '''
+    Dry Run:
+Initialization:
+nums1 = [1, 2, 3, 0, 0, 0]
+nums2 = [2, 5, 6]
+m = 3
+n = 3
+left = m - 1 = 2
+right = n - 1 = 2
+curr = m + n - 1 = 5
+Merging Process:
+First iteration:
+
+Compare nums1[left] = nums1[2] = 3 and nums2[right] = nums2[2] = 6
+Since 3 < 6, set nums1[curr] = nums2[right] = 6
+Update right -= 1 and curr -= 1
+nums1 = [1, 2, 3, 0, 0, 6]
+right = 1, curr = 4
+Second iteration:
+
+Compare nums1[left] = nums1[2] = 3 and nums2[right] = nums2[1] = 5
+Since 3 < 5, set nums1[curr] = nums2[right] = 5
+Update right -= 1 and curr -= 1
+nums1 = [1, 2, 3, 0, 5, 6]
+right = 0, curr = 3
+Third iteration:
+
+Compare nums1[left] = nums1[2] = 3 and nums2[right] = nums2[0] = 2
+Since 3 > 2, set nums1[curr] = nums1[left] = 3
+Update left -= 1 and curr -= 1
+nums1 = [1, 2, 3, 3, 5, 6]
+left = 1, curr = 2
+Fourth iteration:
+
+Compare nums1[left] = nums1[1] = 2 and nums2[right] = nums2[0] = 2
+Since 2 == 2, set nums1[curr] = nums2[right] = 2
+Update right -= 1 and curr -= 1
+nums1 = [1, 2, 2, 3, 5, 6]
+right = -1, curr = 1
+At this point, the right index is -1, indicating that all elements from nums2 have been merged. The remaining elements from nums1 (if any) are already in place.
+    '''
     
     
         
