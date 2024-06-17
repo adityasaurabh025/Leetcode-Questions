9a9e8a65072ed34155245344836e2ae3245493ea
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-06-17 13:35:46 +0530

    Time: 132 ms (16.15%), Space: 20.5 MB (66.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -13,4 +13,66 @@ class Solution:
                 merged[-1][1]= max(merged[-1][1], interval[1])
                 
         return merged
+        """
+        Dry Run with Specific Values:
+Initialization:
+
+arr = [[1, 3], [2, 6], [8, 10], [15, 18]]
+ans = []
+First Iteration (i = 0):
+
+Current interval: arr[0] = [1, 3]
+Since ans is empty, the condition arr[i][0] > ans[-1][1] is not evaluated.
+Add [1, 3] to ans:
+lua
+Copy code
+ans = [[1, 3]]
+Second Iteration (i = 1):
+
+Current interval: arr[1] = [2, 6]
+Compare arr[i][0] and ans[-1][1]:
+arr[i][0] is 2
+ans[-1][1] is 3 (end of last interval in ans)
+Condition: 2 > 3 (False, they overlap)
+Since they overlap, merge the intervals by updating the end value:
+Update ans[-1][1] to max(3, 6) = 6:
+lua
+Copy code
+ans = [[1, 6]]
+Third Iteration (i = 2):
+
+Current interval: arr[2] = [8, 10]
+Compare arr[i][0] and ans[-1][1]:
+arr[i][0] is 8
+ans[-1][1] is 6 (end of last interval in ans)
+Condition: 8 > 6 (True, no overlap)
+Since there's no overlap, add [8, 10] to ans:
+lua
+Copy code
+ans = [[1, 6], [8, 10]]
+Fourth Iteration (i = 3):
+
+Current interval: arr[3] = [15, 18]
+Compare arr[i][0] and ans[-1][1]:
+arr[i][0] is 15
+ans[-1][1] is 10 (end of last interval in ans)
+Condition: 15 > 10 (True, no overlap)
+Since there's no overlap, add [15, 18] to ans:
+lua
+
+ans = [[1, 6], [8, 10], [15, 18]]
+Summary of Key Values:
+First Iteration: No overlap check since ans is empty.
+Second Iteration:
+Compare arr[1][0] = 2 with ans[-1][1] = 3
+Condition: 2 > 3 (False, they overlap)
+Third Iteration:
+Compare arr[2][0] = 8 with ans[-1][1] = 6
+Condition: 8 > 6 (True, no overlap)
+Fourth Iteration:
+Compare arr[3][0] = 15 with ans[-1][1] = 10
+Condition: 15 > 10 (True, no overlap)
+
+The condition arr[i][0] > ans[-1][1] checks if the start of the current interval (arr[i][0]) is greater than the end of the last interval in ans (ans[-1][1]). If true, it means there's no overlap, and the current interval can be added to ans. If false, it means there's an overlap, and the intervals need to be merged.
         
+        """
