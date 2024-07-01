4e1b1a5d68dd9abbfb365ac793e05a45c023819b
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-07-01 13:05:50 +0530

    Time: 35 ms (73.95%), Space: 16.3 MB (93.46%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -5,12 +5,37 @@ class Solution:
         """
         
         n= len(nums)
-        temp=nums
-        for i in range(n):
-            for j in range(i+1, n):
-                if nums[i]>nums[j]:
-                    temp=nums[i]
-                    nums[i]=nums[j]
-                    nums[j]=temp
+       # since size of array is fixed we can count the ocurance and then adjust the array based on count
+        count_0=0
+        count_1=0
+        count_2=0
+        
+        for num in nums:
+            if num==0:
+                count_0+=1
+            elif num==1:
+                count_1+=1
+            elif num==2:
+                count_2+=1
+            
+        nums[:count_0]=[0]* count_0
+        nums[count_0: count_0+count_1]=[1]* count_1
+        nums[count_0+count_1:]=[2]*count_2
+        
+        return nums
+                
+                
+                
+                
+                
+#         temp=nums
+#         for i in range(n):
+#             for j in range(i+1, n):
+#                 if nums[i]>nums[j]:
+#                     temp=nums[i]
+#                     nums[i]=nums[j]
+#                     nums[j]=temp
                     
-        return temp
+#         return temp
+
+        
