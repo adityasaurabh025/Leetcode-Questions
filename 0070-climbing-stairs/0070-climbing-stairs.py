3de4b69ef6a17b7dbe842c90cad27b7ade8893fb
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-07-02 15:33:24 +0530

    Time: 37 ms (39.44%), Space: 16.4 MB (58.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,11 +3,52 @@ class Solution:
         if n<=1:
             return 1
         
-        dp=[0]*(n+1)
-        dp[0]=1
-        dp[1]=1
+        prev1=1
+        prev2=1
         
         for i in range(2, n+1):
-            dp[i]= dp[i-1]+ dp[i-2]
+            curr=prev1+prev2
+            prev2=prev1
+            prev1=curr
         
-        return dp[n]
+        return prev1
+        
+#         dp=[0]*(n+1)
+#         dp[0]=1
+#         dp[1]=1
+        
+#         for i in range(2, n+1):
+#             dp[i]= dp[i-1]+ dp[i-2]
+        
+#         return dp[n]
+#     #TC- O(n), SC- O(n)
+    
+    
+    
+    """
+    dp[0] = 1: There is one way to stay at the ground (do nothing).
+    dp[1] = 1: There is one way to reach the first step (one single step).
+    
+    n = 5
+dp = [0, 0, 0, 0, 0, 0] (size n + 1)
+Set dp[0] = 1 and dp[1] = 1
+Now dp = [1, 1, 0, 0, 0, 0]
+Iteration:
+
+For i = 2:
+dp[2] = dp[1] + dp[0] = 1 + 1 = 2
+Now dp = [1, 1, 2, 0, 0, 0]
+For i = 3:
+dp[3] = dp[2] + dp[1] = 2 + 1 = 3
+Now dp = [1, 1, 2, 3, 0, 0]
+For i = 4:
+dp[4] = dp[3] + dp[2] = 3 + 2 = 5
+Now dp = [1, 1, 2, 3, 5, 0]
+For i = 5:
+dp[5] = dp[4] + dp[3] = 5 + 3 = 8
+Now dp = [1, 1, 2, 3, 5, 8]
+Result:
+
+Return dp[5] which is 8
+    
+    """
