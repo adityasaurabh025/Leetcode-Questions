5994e58c1f2ac239792e3d34055ab264734be278
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-07-02 11:15:05 +0530

    Time: 481 ms (59.97%), Space: 41.7 MB (62.36%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -3,12 +3,12 @@ class Solution:
         m= len(text1)+1
         n= len(text2)+1
         
-        dp=[[0]* (n) for _ in range(m)]
+        dp=[[0]* (n) for _ in range(m)] # fill zero in array
         
         for i in range(1, m):
             for j in range(1, n):
                 if text1[i-1]==text2[j-1]:
-                    dp[i][j]= 1+ dp[i-1][j-1]
+                    dp[i][j]= 1+ dp[i-1][j-1] # if chars are matching, increase the count and decrease the pointers
                 else:
-                    dp[i][j]=max(dp[i-1][j], dp[i][j-1])
-        return dp[-1][-1]
+                    dp[i][j]=max(dp[i-1][j], dp[i][j-1])# if array is not match then take max of both observations
+        return dp[-1][-1] # return -1 if not present
