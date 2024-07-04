3710e7eb25fdfb508ff34b4eb9f4d89b1a2ac15e
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-07-05 00:36:11 +0530

    Time: 41 ms (69.99%), Space: 16.8 MB (86.15%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -18,6 +18,29 @@ class Solution:
 #             if nums[i]< mini:
 #                 mini=nums[i]
 #         return mini
+"""
+For the array [4,5,6,7,0,1,2]:
+
+Initial left = 0, right = 6, mid = 3:
+nums[mid] = 7, nums[right] = 2:
+Since nums[mid] > nums[right], set left = mid + 1 = 4.
+Now, left = 4, right = 6, mid = 5:
+nums[mid] = 1, nums[right] = 2:
+Since nums[mid] <= nums[right], set right = mid = 5.
+Now, left = 4, right = 5, mid = 4:
+nums[mid] = 0, nums[right] = 1:
+Since nums[mid] <= nums[right], set right = mid = 4.
+Now, left = 4, right = 4:
+The loop terminates, and nums[left] = nums[4] = 0 is the minimum element.
+This approach ensures that the solution runs in 
+\U0001d442
+(
+log
+⁡
+\U0001d45b
+)
+O(logn) time, making it efficient for large arrays.
+"""
           
           
         
