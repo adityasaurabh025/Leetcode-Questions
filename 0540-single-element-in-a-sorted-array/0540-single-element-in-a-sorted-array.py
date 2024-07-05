ea14b46f52dd4db6c51c834c37592e241ab1dce5
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-07-06 00:21:14 +0530

    Time: 148 ms (14.11%), Space: 26.7 MB (46.33%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,14 +1,44 @@
 class Solution:
-    def singleNonDuplicate(self, nums: List[int]) -> int:
-        freq={}
-        
-        for num in nums:
-            if num in freq:
-                freq[num]+=1
+    def singleNonDuplicate(self, arr: List[int]) -> int:
+        low, high = 0, len(arr) - 1
+
+        while low < high:
+            mid = (low + high) // 2
+
+        # Check if mid is even or odd to determine the pair check
+            if mid % 2 == 0:
+                if arr[mid] == arr[mid + 1]:
+                    low = mid + 2
+                else:
+                    high = mid
             else:
-                freq[num]=1
+                if arr[mid] == arr[mid - 1]:
+                    low = mid + 1
+                else:
+                    high = mid
+
+        return arr[low]
         
-        for num in nums:
-            if freq[num]==1:
-                return num
-        return -1
+        
+        
+        
+        
+        
+#         freq={}
+        
+#         for num in nums:
+#             if num in freq:
+#                 freq[num]+=1
+#             else:
+#                 freq[num]=1
+        
+#         for num in nums:
+#             if freq[num]==1:
+#                 return num
+#         return -1
+#         TC:- O(N), SC- O(N)
+
+            
+            
+            
+        
