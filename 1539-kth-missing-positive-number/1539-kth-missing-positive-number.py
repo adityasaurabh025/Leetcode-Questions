b4b71808f6f9597fa7134871d655061cb17e96b5
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-07-09 13:31:31 +0530

    Time: 41 ms (96.34%), Space: 16.6 MB (56.81%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -6,3 +6,19 @@ class Solution:
             else:
                 break
         return k
+    
+    """
+    Incrementing k Based on Array Elements:
+
+The loop iterates through each element arr[i] in the array arr.
+If arr[i] <= k, it means that arr[i] is a number that exists in the sequence up to k (or possibly the next missing number).
+Therefore, k is incremented by 1 (k += 1).
+Breaking the Loop:
+
+As soon as an element arr[i] is encountered that is greater than k, the loop breaks (break statement).
+This indicates that arr[i] is the first number encountered in arr that is greater than k, meaning k has reached or surpassed the count of positive integers up to the kth missing positive integer.
+Returning k:
+
+After exiting the loop, the current value of k is returned.
+This value of k represents the count of positive integers (including the missing ones) up to the kth missing positive integer from arr.
+    """
