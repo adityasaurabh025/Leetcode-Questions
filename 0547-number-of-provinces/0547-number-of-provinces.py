9893f7ebc2705c45b2e1ffd74061a0024ab3a230
Aditya Saurabh <101804303+adityasaurabh025@users.noreply.github.com>
2024-08-07 17:07:22 +0530

    Time: 184 ms (56.25%), Space: 17.3 MB (77.85%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,18 +1,44 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        def dfs(i):
-            for j in range(len(isConnected)):
-                if isConnected[i][j]==1 and not visited[j]:
-                    visited[j]= True
-                    dfs(j)
+        def find(x):
+            if parent[x]!=x:
+                parent[x]= find(parent[x])
+            return parent[x]
         
+        def union(x, y):
+            rootX=find(x)
+            rootY= find(y)
+            if rootX!= rootY:
+                parent[rootY]=rootX
         n= len(isConnected)
-        visited = [False]* n
-        
-        province=0
+        parent= list(range(n))
         
         for i in range(n):
-            if not visited[i]:
-                dfs(i)
-                province+=1
+            for j in range(i+1, n):
+                if isConnected[i][j]==1:
+                    union(i, j)
+        province= len(set(find(i) for i in range(n)))
         return province
+
+        
+        
+        
+        
+        
+#         def dfs(i):
+#             for j in range(len(isConnected)):
+#                 if isConnected[i][j]==1 and not visited[j]:
+#                     visited[j]= True
+#                     dfs(j)
+        
+#         n= len(isConnected)
+#         visited = [False]* n
+        
+#         province=0
+        
+#         for i in range(n):
+#             if not visited[i]:
+#                 dfs(i)
+#                 province+=1
+#         return province
+
